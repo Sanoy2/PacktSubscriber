4e6894ec1faf56889eab4fd2024fea716d26ad9b
Krzysztof_Tomkow <218514@student.pwr.edu.pl>
2018-10-18 21:28:32 +0200

    Some classes created

---
+++ TitleFinder.py
@@ -0,0 +1,5 @@
+from bs4 import BeautifulSoup
+
+class TitleFinder():
+    def FindTitle(self, html):
+        return "not implemented yet"
---
+++ TitleGetter.py
@@ -0,0 +1,21 @@
+from MyHTMLParser import MyHTMLParser
+from HTMLGetter import HTMLGetter
+from TitleFinder import TitleFinder
+
+class TitleGetter():
+    def __init__(self):
+        self.htmlParser = MyHTMLParser()
+        self.url = "https://www.packtpub.com/packt/offers/free-learning"
+    
+    def GetTitle(self):
+        html = self.GetHtml()
+        finder = TitleFinder()
+        title = finder.FindTitle(html)
+        return title
+
+    
+    def GetHtml(self):
+        Getter = HTMLGetter()
+        html = Getter.GetHTML(self.url)
+        return html
+
