b80fbf05a9d9c595616800329f774a38f039dffb
Krzysztof_Tomkow <krzysztof.tomkow@gmail.com>
2018-10-20 20:37:18 +0200

    Changed schedule to send mails at 10:00 am

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Subscriber import Subscriber
 
 def main():
     subscriber = Subscriber()
-    subscriber.Start("20:18") 
+    subscriber.Start("10:00") 
 
 if __name__ == "__main__":
     main()
