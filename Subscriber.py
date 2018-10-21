680b65ee18c9b932b70f3682fa4351344282eecb
Krzysztof_Tomkow <krzysztof.tomkow@gmail.com>
2018-10-21 19:16:18 +0200

    Created requirements.txt

--- Subscriber.py
+++ Subscriber.py
@@ -14,6 +14,7 @@ class Subscriber:
             schedule.run_pending()
             time.sleep(1)
 
+
     def DoTheJob(self):
         title = self.GetTitle()
         self.SendMail(title)
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from Subscriber import Subscriber
 
 def main():
     subscriber = Subscriber()
-    subscriber.Start("10:00") 
+    #subscriber.Start("10:00") 
+    subscriber.DoTheJob()
 
 if __name__ == "__main__":
     main()
