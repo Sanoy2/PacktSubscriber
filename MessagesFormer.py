e891b55981ae57ba6e718aa4e6d5fd0c61c1ecc7
Krzysztof_Tomkow <krzysztof.tomkow@gmail.com>
2018-10-22 21:55:44 +0200

    Split forming messages to MessageFormer and MessagesFormer

---
+++ MessagesFormer.py
@@ -0,0 +1,24 @@
+from MessageFormer import MessageFormer
+from FileReader import FileReader
+
+class MessagesFormer():
+    def FormAllMessages(self, ebookTitle):
+        messageFormer = MessageFormer()
+        addressees = self.GetAddressees()
+        messages = []
+        senderAddress = self.GetSenderAddress()
+
+        for address in addressees:
+            newMessage = messageFormer.FormMessage(ebookTitle, senderAddress, address)
+            messages.append(newMessage)
+        return messages
+
+    def GetAddressees(self):
+        fileReader = FileReader()
+        addressees = fileReader.ReadLines("addressees.txt")
+        return addressees
+
+    def GetSenderAddress(self):
+        fileReader = FileReader()
+        address = fileReader.ReadLines("loginData.txt")[0]
+        return address
